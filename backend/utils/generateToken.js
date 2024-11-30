e57b9735ba1c16d08bf7b6e244654cba0abb8be8
blacktigerq8 <blacktigerq8@gmail.com>
2024-11-30 17:29:53 +0300

    Fixed enhanced the navigation logic, and initiated category form...

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,14 +6,14 @@ const generateToken = (res, userId) => {
     return;
   }
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "1d",
+    expiresIn: "3d",
   });
 
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
     sameSite: "strict", // Prevent CSRF attacks
-    maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
 
