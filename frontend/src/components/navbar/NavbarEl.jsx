ed763ad5db3d4f6ecc04e54bf786ce6df17dc100
blacktigerq8 <blacktigerq8@gmail.com>
2024-11-30 18:02:46 +0300

    Enhanced the close of the sidebar.

--- frontend/src/components/navbar/NavbarEl.jsx
+++ frontend/src/components/navbar/NavbarEl.jsx
@@ -41,8 +41,8 @@ export const Overlay = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
+  width: 100dvw;
+  height: 100dvh;
   background-color: rgba(0, 0, 0, 0);
   z-index: 0;
   display: ${(props) => (props.isOpen ? "block" : "none")};
